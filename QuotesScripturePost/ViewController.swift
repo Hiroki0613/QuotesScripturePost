b283af206b5fe922e5fff87bfbd7f00321479c59
Hiroki Kondo <kondo.matr02@gmail.com>
2020-03-14 15:58:42 +0900

    Initial Commit

---
+++ QuotesScripturePost/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuotesScripturePost
+//
+//  Created by 近藤宏輝 on 2020/03/14.
+//  Copyright © 2020 Hiroki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
