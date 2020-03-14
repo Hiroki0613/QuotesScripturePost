9290d7fd0d03d402913ddda9b58bc0bb668aabda
Hiroki Kondo <kondo.matr02@gmail.com>
2020-03-14 16:45:20 +0900

    タブバーを実装(プロパティーは未設定)。

---
+++ QuotesScripturePost/BellVC.swift
@@ -0,0 +1,22 @@
+//
+//  BellViewController.swift
+//  QuotesScripturePost
+//
+//  Created by 近藤宏輝 on 2020/03/14.
+//  Copyright © 2020 Hiroki. All rights reserved.
+//
+
+import UIKit
+
+class BellVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBlue
+    }
+    
+
+
+
+}
