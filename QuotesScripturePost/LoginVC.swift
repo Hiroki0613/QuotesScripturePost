6dc77068d7c771d73197fd921f2752c671d11fa0
Hiroki Kondo <kondo.matr02@gmail.com>
2020-03-15 18:40:10 +0900

    LogInVCにボタンを実装

--- QuotesScripturePost/LoginVC.swift
+++ QuotesScripturePost/LoginVC.swift
@@ -11,13 +11,16 @@ import UIKit
 class LoginVC: UIViewController {
     
     let backgroundView = UIImageView()
-    let loginButton = QSButton(view: self, x: 100, y: 100, title: "にほんご")
+    let loginButton = QSButton(title: "LOGIN")
+    let backToHomeButton = QSButton(title: "Back to HOME")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureBackgroundView()
-        view.addSubview(loginButton)
+        configureLoginButton()
+        configureBackToHomeButton()
     }
+    
     
     func configureBackgroundView() {
         view.addSubview(backgroundView)
@@ -31,6 +34,51 @@ class LoginVC: UIViewController {
             backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
+    }
+    
+    
+    func configureLoginButton(){
+        view.addSubview(loginButton)
+        loginButton.addTarget(self, action: #selector(loginToPost), for: .touchUpInside)
+        
+        let padding:CGFloat = 40
+        
+        NSLayoutConstraint.activate([
+            loginButton.topAnchor.constraint(equalTo: view.centerYAnchor, constant: -view.frame.height/14),
+            loginButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            loginButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            loginButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    @objc func loginToPost() {
+        //ログイン後に、投稿画面に遷移
+        
+        //ログイン(すでにログインしている場合は、この動作をパス)
+        
+        //投稿画面に遷移
+        
+    }
+    
+    
+    func configureBackToHomeButton(){
+        view.addSubview(backToHomeButton)
+        backToHomeButton.addTarget(self, action: #selector(backToHome), for: .touchUpInside)
+        
+        let padding:CGFloat = 40
+        
+        NSLayoutConstraint.activate([
+            backToHomeButton.topAnchor.constraint(equalTo: view.centerYAnchor, constant: view.frame.height/14),
+            backToHomeButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            backToHomeButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            backToHomeButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    
+    @objc func backToHome() {
+        //Home画面に戻る
+        dismiss(animated: true, completion: nil)
     }
 
 
--- QuotesScripturePost/QSButton.swift
+++ QuotesScripturePost/QSButton.swift
@@ -20,12 +20,20 @@ class QSButton: UIButton {
         fatalError("init(coder:) has not been implemented")
     }
     
-    init(view:UIView, x:CGFloat, y:CGFloat, title: String){
+//    init(view:UIView, x:CGFloat, y:CGFloat, title: String){
+//        super.init(frame: .zero)
+//        self.backgroundColor = .systemBlue
+//        self.setTitle(title, for: .normal)
+//        frame = CGRect(x: x, y: y, width: view.frame.size.width * 8 / 10 , height: view.frame.height / 10)
+//    }
+    
+    init(title:String) {
         super.init(frame: .zero)
         self.backgroundColor = .systemBlue
         self.setTitle(title, for: .normal)
-        frame = CGRect(x: x, y: y, width: view.frame.size.width * 8 / 10 , height: view.frame.height / 10)
+        configure()
     }
+    
     
     //QSButtonを作成するためのプロパティを定義
     private func configure(){
