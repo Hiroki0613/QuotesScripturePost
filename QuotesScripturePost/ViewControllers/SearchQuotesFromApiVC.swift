0c1b207ae145981af356e94fb33b6919804db913
Hiroki Kondo <kondo.matr02@gmail.com>
2020-03-16 22:45:37 +0900

    searchQuoteLabel,quoteTextFieldを実装

--- QuotesScripturePost/ViewControllers/SearchQuotesFromApiVC.swift
+++ QuotesScripturePost/ViewControllers/SearchQuotesFromApiVC.swift
@@ -11,29 +11,63 @@ import UIKit
 class SearchQuotesFromApiVC: UIViewController {
     
     let postImage = UIImageView()
+    let searchQuoteLabel = QSLabel(textAlignment: .center, string: "Search Quotes")
+    let quoteTextField = QSTextField()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         configurePostImage()
+        configureLabel()
+        configureTextField()
     }
     
     
     func configurePostImage() {
-           self.view.addSubview(postImage)
-           postImage.translatesAutoresizingMaskIntoConstraints = false
-           
-           //暫定で桜の画像を入れている
-           postImage.image = UIImage(named: "sakura")
-           postImage.contentMode = .scaleToFill
-           
-           NSLayoutConstraint.activate([
-               postImage.topAnchor.constraint(equalTo: view.topAnchor),
-               postImage.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-               postImage.heightAnchor.constraint(equalToConstant: view.frame.width),
-               postImage.heightAnchor.constraint(equalToConstant: view.frame.width)
-           ])
-       }
+        self.view.addSubview(postImage)
+        postImage.translatesAutoresizingMaskIntoConstraints = false
+        
+        //暫定で桜の画像を入れている
+        postImage.image = UIImage(named: "sakura")
+        postImage.contentMode = .scaleToFill
+        
+        NSLayoutConstraint.activate([
+            postImage.topAnchor.constraint(equalTo: view.topAnchor),
+            postImage.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            postImage.heightAnchor.constraint(equalToConstant: view.frame.width),
+            postImage.heightAnchor.constraint(equalToConstant: view.frame.width)
+        ])
+    }
+    
+    func configureLabel() {
+        self.view.addSubview(searchQuoteLabel)
+        searchQuoteLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        let padding:CGFloat = 20
+        
+        NSLayoutConstraint.activate([
+            searchQuoteLabel.topAnchor.constraint(equalTo: postImage.bottomAnchor, constant: padding),
+            searchQuoteLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            searchQuoteLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            searchQuoteLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+    }
+    
+    
+    func configureTextField() {
+        self.view.addSubview(quoteTextField)
+        quoteTextField.translatesAutoresizingMaskIntoConstraints = false
+        
+        let padding:CGFloat = 20
+        
+        NSLayoutConstraint.activate([
+            quoteTextField.topAnchor.constraint(equalTo: searchQuoteLabel.bottomAnchor, constant: padding),
+            quoteTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            quoteTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            quoteTextField.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+    }
 
 
 }
