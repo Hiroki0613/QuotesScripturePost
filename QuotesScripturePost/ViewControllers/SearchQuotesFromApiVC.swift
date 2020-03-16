ca2a276e07d7f0b67b26ea39ccee57f2bc031f05
Hiroki Kondo <kondo.matr02@gmail.com>
2020-03-16 22:17:33 +0900

    postImageを追加

--- QuotesScripturePost/ViewControllers/SearchQuotesFromApiVC.swift
+++ QuotesScripturePost/ViewControllers/SearchQuotesFromApiVC.swift
@@ -9,14 +9,31 @@
 import UIKit
 
 class SearchQuotesFromApiVC: UIViewController {
+    
+    let postImage = UIImageView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .systemPink
-
+        view.backgroundColor = .systemBackground
+        configurePostImage()
     }
     
+    
+    func configurePostImage() {
+           self.view.addSubview(postImage)
+           postImage.translatesAutoresizingMaskIntoConstraints = false
+           
+           //暫定で桜の画像を入れている
+           postImage.image = UIImage(named: "sakura")
+           postImage.contentMode = .scaleToFill
+           
+           NSLayoutConstraint.activate([
+               postImage.topAnchor.constraint(equalTo: view.topAnchor),
+               postImage.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+               postImage.heightAnchor.constraint(equalToConstant: view.frame.width),
+               postImage.heightAnchor.constraint(equalToConstant: view.frame.width)
+           ])
+       }
 
 
 }
