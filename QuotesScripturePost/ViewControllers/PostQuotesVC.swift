cf8b0e6ff2a448cac1b812ad6565bd85a373151e
Hiroki Kondo <kondo.matr02@gmail.com>
2020-03-19 21:39:47 +0900

    PostQuotesVCを追加

---
+++ QuotesScripturePost/ViewControllers/PostQuotesVC.swift
@@ -0,0 +1,25 @@
+//
+//  PostQuotesVC.swift
+//  QuotesScripturePost
+//
+//  Created by 近藤宏輝 on 2020/03/19.
+//  Copyright © 2020 Hiroki. All rights reserved.
+//
+
+
+//ここで選んだ名言を任意の位置に貼り付けて、問題がなければFireBaseへPostする。
+//そのあとに、dismissをして、投稿画面に戻る
+
+import UIKit
+
+class PostQuotesVC: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
