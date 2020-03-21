73cbb8c0b7a8ef33e753ab73bd5d04b1190876b6
Hiroki Kondo <kondo.matr02@gmail.com>
2020-03-21 21:08:48 +0900

    CommunicationTableCellを追加

---
+++ QuotesScripturePost/Parts/CommunicationTableCell.swift
@@ -0,0 +1,16 @@
+//
+//  CommunicationTableCell.swift
+//  QuotesScripturePost
+//
+//  Created by 近藤宏輝 on 2020/03/21.
+//  Copyright © 2020 Hiroki. All rights reserved.
+//
+
+import UIKit
+
+class CommunicationTableCell: UITableViewCell {
+    
+    
+    
+    
+}
