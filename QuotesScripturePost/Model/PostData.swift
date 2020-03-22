1a79f7bd421255a6cd72c1400ea5e3a0c8bb89ea
Hiroki Kondo <kondo.matr02@gmail.com>
2020-03-22 23:45:00 +0900

    PostDataのModelを実装

--- QuotesScripturePost/Model/PostData.swift
+++ QuotesScripturePost/Model/PostData.swift
@@ -9,8 +9,8 @@
 import Foundation
 import FirebaseDatabase
 
+//画像の投稿データ一覧
 struct Photo {
-    
     //投稿ID
     var postId:String?
     //プロフィール画像
@@ -30,5 +30,30 @@ struct Photo {
     //投稿コメント
     var postComment:String?
     
+    //PostDataの初期設定
+    init?(snapshot: DataSnapshot) {
+        
+        let postData = snapshot.value as? [String:String]
+        
+        if let postData = postData {
+            let postId = snapshot.key
+            let byPrfilePhotoUrl = postData["byPrfilePhotoUrl"]
+            let byusernameID = postData["byusernameID"]
+            let byUsername = postData["byUsername"]
+            let date = postData["date"]
+            let byPostUrl = postData["byPostUrl"]
+            let postComment = postData["postComment"]
+            
+            guard byPrfilePhotoUrl != nil && byusernameID != nil && byUsername != nil && date != nil && byPostUrl != nil && postComment != nil else { return nil }
+            
+            self.postId = postId
+            self.byPrfilePhotoUrl = byPrfilePhotoUrl
+            self.byusernameID = byusernameID
+            self.byUsername = byUsername
+            self.date = date
+            self.byPostUrl = byPostUrl
+            self.postComment = postComment
+        }
+    }
 }
 
