15dbaf07a43bd2fcce7ceb2a98fcc5c89a854f9c
Hiroki Kondo <kondo.matr02@gmail.com>
2020-03-22 20:07:31 +0900

    UserServiceを実装

---
+++ QuotesScripturePost/Model/PhotoUser.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoUser.swift
+//  QuotesScripturePost
+//
+//  Created by 近藤宏輝 on 2020/03/22.
+//  Copyright © 2020 Hiroki. All rights reserved.
+//
+
+import Foundation
+
+//ユーザー情報
+struct PhotoUser {
+    var userID:String?
+    var username:String?
+}
--- QuotesScripturePost/Services/UserService.swift
+++ QuotesScripturePost/Services/UserService.swift
@@ -7,3 +7,53 @@
 //
 
 import Foundation
+import FirebaseDatabase
+
+class UserService {
+    
+    //ユーザー情報を作成
+    static func createUserProfile(userId:String, username:String, completion: @escaping (PhotoUser?) -> Void) {
+        
+        // ユーザー情報のdictionaryを作成
+        let userProfileData = ["username":username]
+        //databaseからrefを取得
+        let ref = Database.database().reference()
+        // UserIDからプロフィールを作成
+        ref.child("users").child(userId).setValue(userProfileData) { (error, ref) in
+            
+            if error != nil {
+                //エラーが発生
+                completion(nil)
+            } else {
+                //ユーザー情報を作成して、return
+                let u = PhotoUser(userID: userId, username: username)
+                completion(u)
+            }
+        }
+    }
+    
+    
+    //ユーザー情報を取得(ログイン時に使用)
+    static func getUserProfile(userId:String, completion: @escaping (PhotoUser?) -> Void) {
+        
+        //databaseからrefを取得
+        let ref = Database.database().reference()
+        ref.child("users").child(userId).observeSingleEvent(of: .value) { (snapshot) in
+            
+            //snapshotに入っている、ユーザー情報が入っているかを確認する
+            if let userProfileData = snapshot.value as? [String: Any] {
+                
+                //ユーザー情報が入っていることを確認
+                var u = PhotoUser()
+                u.userID = snapshot.key
+                u.username = userProfileData["username"] as? String
+                
+                //取得したユーザー情報を入力して、return
+                completion(u)
+            } else {
+                //ユーザー情報は入っていない
+                completion(nil)
+            }
+        }
+    }
+}
