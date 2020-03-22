58870afe86a717e6aff5d877c8b4aed77111f5bf
Hiroki Kondo <kondo.matr02@gmail.com>
2020-03-22 23:24:18 +0900

    Photoのモデルを実装

--- QuotesScripturePost/Model/PostData.swift
+++ QuotesScripturePost/Model/PostData.swift
@@ -7,4 +7,28 @@
 //
 
 import Foundation
+import FirebaseDatabase
+
+struct Photo {
+    
+    //投稿ID
+    var postId:String?
+    //プロフィール画像
+    var byPrfilePhotoUrl:String?
+    //ユーザーID
+    var byusernameID:String?
+    //ユーザー名
+    var byUsername:String?
+    //投稿日付
+    var date:String?
+    //投稿画像
+    var byPostUrl:String?
+    //ハートの数
+    var heartCount:Int?
+    //コメントの数
+    var commentCount:Int?
+    //投稿コメント
+    var postComment:String?
+    
+}
 
